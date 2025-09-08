4cbd1de1d7464d7dcaaeae16b18e4bbf74a9e1f9
Deepansh Gupta <deepansh.gup124@gmail.com>
2025-09-08 23:56:48 +0530

    js

---
+++ printing_num.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 20;
+let c = 30;
+
+let largest;
+
+if (a >= b && a >= c) {
+  largest = a;
+} else if (b >= a && b >= c) {
+  largest = b;
+} else {
+  largest = c;
+}
+
+console.log("The largest number is: " + largest);
