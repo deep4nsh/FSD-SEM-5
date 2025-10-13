9062d74255eb67ab756ce21d2c40309a69dfefdd
deepansh <deepansh.gup124@gmail.com>
2025-10-13 12:20:04 +0530

    dom

---
+++ javascript/ES6/dom.js
@@ -0,0 +1,12 @@
+const div=document.createElement('div');
+const h1=document.createElement('h1');
+h1.innerText='abes';
+div.appendChild(h1);
+document.getElementById('root').appendChild(div);
+
+// div.innerHTML=`
+//     <div>
+//         <h1>abes</h1>
+//     </div>
+// `
+// document.getElementById('root').appendChild(div);
